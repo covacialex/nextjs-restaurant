bd7fabeb839a4513f42ac3e5a38942cdbb83441f
Covaci Alex <alxcovaci96@gmail.com>
2022-10-21 11:41:28 +0300

    Menu, nav, footer responsive

--- components/Navigation/NavInfo.jsx
+++ components/Navigation/NavInfo.jsx
@@ -12,9 +12,9 @@ const NavInfo = () => {
         <div className={classes.left}>
           <div className={classes.address}>
             <HiLocationMarker className={classes.icon} />
-            <address>Str Alexandru Ciurea 6, Cluj-Napoca 400000</address>
+            <address>Str Alexandru Ciurea 6, Cluj-Napoca</address>
           </div>
-          <BsDiamond className={classes.icon} />
+          <BsDiamond className={classes.icon__diamond} />
           <div className={classes.hours}>
             <BsClockFill className={classes.icon} />
             <p>Daily: 6:00 am to 10:00 pm</p>
@@ -26,7 +26,7 @@ const NavInfo = () => {
             <AiFillPhone className={classes.icon} />
             <a href="tel:+40-752-988-352">+40 752 988 352</a>
           </div>
-          <BsDiamond className={classes.icon} />
+          <BsDiamond className={classes.icon__diamond} />
           <div className={classes.mail}>
             <AiFillMail className={classes.icon} />
             <a href="mailto: restaurant@delicious.com">
